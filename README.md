4246006b14dd76b5b94a2dcdb513c68a5ac63702
Anton Tolchanov <commits@knyar.net>
2020-10-17 16:09:33 +0100

    Add a script to update software on a running bridge

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # berg-bridge-dump
+
+To update bridge software, run `./update.sh <ipaddr>` (where `<ipaddr>` is the
+IP address of your bridge).
---
+++ update.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# vim: ts=2:sw=2:sts=2:expandtab
+
+set -e -u -o pipefail
+
+COPY='usr/local/bergcloud-bridge usr/bin/oneshot_bergcloud_bridge.sh'
+
+BRIDGE=${1:-}
+BASEDIR="$(cd "$(dirname "$0")"; pwd -P)"
+
+if [[ -z "${BRIDGE}" ]]; then
+  echo "Usage: $0 <bridge-ip>"
+  exit 3
+fi
+
+for FILE in $COPY; do
+  if [[ ! -e "${BASEDIR}/${FILE}" ]]; then
+    echo "${BASEDIR}/${FILE} not found"
+    exit 3
+  fi
+done
+
+echo "Remounting root filesystem in read/write"
+ssh root@${BRIDGE} "mount -o remount,rw /"
+
+for FILE in $COPY; do
+  echo "Copying $FILE"
+  ssh root@${BRIDGE} "rm -rf /${FILE}.new"
+  scp -rp "${BASEDIR}/${FILE}" "root@${BRIDGE}:/${FILE}.new"
+  echo "Backing up old $FILE"
+  ssh root@${BRIDGE} "rm -rf /${FILE}.bak && mv /${FILE} /${FILE}.bak"
+  echo "Moving $FILE in place"
+  ssh root@${BRIDGE} "mv /${FILE}.new /${FILE}"
+done
+
+echo "All done. Please reboot the bridge."
