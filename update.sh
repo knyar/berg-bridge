86af67fbc5619b66c8c6fc4628fe179d801a99a1
Anton Tolchanov <commits@knyar.net>
2020-12-25 13:13:17 +0000

    Preserve /usr/local/bergcloud-bridge/endpoint.override

--- update.sh
+++ update.sh
@@ -4,9 +4,11 @@
 set -e -u -o pipefail
 
 COPY='usr/local/bergcloud-bridge usr/bin/oneshot_bergcloud_bridge.sh'
+BACKUP='/usr/local/bergcloud-bridge/endpoint.override'
 
 BRIDGE=${1:-}
 BASEDIR="$(cd "$(dirname "$0")"; pwd -P)"
+TMP_PREFIX="/tmp/$RANDOM"
 
 if [[ -z "${BRIDGE}" ]]; then
   echo "Usage: $0 <bridge-ip>"
@@ -23,6 +25,12 @@ done
 echo "Remounting root filesystem in read/write"
 ssh root@${BRIDGE} "mount -o remount,rw /"
 
+for FILE in $BACKUP; do
+  TMPFILE="${TMP_PREFIX}${FILE##*/}"
+  echo "Backing up $FILE"
+  ssh root@${BRIDGE} "test -e $FILE && cp $FILE $TMPFILE"
+done
+
 for FILE in $COPY; do
   echo "Copying $FILE"
   ssh root@${BRIDGE} "rm -rf /${FILE}.new"
@@ -31,6 +39,12 @@ for FILE in $COPY; do
   ssh root@${BRIDGE} "rm -rf /${FILE}.bak && mv /${FILE} /${FILE}.bak"
   echo "Moving $FILE in place"
   ssh root@${BRIDGE} "mv /${FILE}.new /${FILE}"
+done
+
+for FILE in $BACKUP; do
+  TMPFILE="${TMP_PREFIX}${FILE##*/}"
+  echo "Restoring $FILE"
+  ssh root@${BRIDGE} "test -e $TMPFILE && cp $TMPFILE $FILE"
 done
 
 echo "All done. Please reboot the bridge."
